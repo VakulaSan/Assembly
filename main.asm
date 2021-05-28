49f24922ffac44454632dc3575bb89159b353c49
Syncrone <vakulasan@gmail.com>
2021-05-29 01:17:36 +0300

    29.05.2021 home

--- main.asm
+++ main.asm
@@ -1,30 +1,38 @@
 ;======OLED Screen on SSD1306====
 .device ATmega328P
 .include "headers.inc"
-
-		.cseg
-		rjmp init
-		wait:   push    temp
-        check:  lds     temp,   TWCR
-        sbrs    temp,   TWINT
-		rjmp    check
-		lds     temp,   TWSR 
- 		sts     UDR0,   temp
-		pop     temp
-        ret 
-		send_slaw:
-        in      ZL,     SPL
-		in      ZH,     SPH
-		ldd     temp,   Z+1
-		sts     TWDR,   temp
-		ldi     temp,   1<<TWEN | 1<<TWINT
-		sts     TWCR,   temp
-		rcall wait
-		ret	   
-        SendSLAW_ 0x78
+         
+init:     Initialization
+		SendStart
+          SendSLAW 0x78
+		;-----Initialization of display
+		SendCommand  0xA8          ;Set MUX ratio 3F
+		SendCommand  0x3F
+		SendCommand  0xD3          ;Set display offset
+		SendCommand  0x00
+          SendCommand  0x40          ;Display start line 40-7F
+		SendCommand  0xA0          ;Set Segment Re-map
+		SendCommand  0xC0          ;Set COM port output Scan detection
+		SendCommand  0xDA          ;Set COM Pins Hardware Configuration
+		SendCommand  0x12
+		SendCommand  0x81          ;Set contrast control 00-FF
+	     SendCommand  0xFF
+		SendCommand  0xA4          ;Entire display ON
+		SendCommand  0xA7          ;Set normal inverse display
+		SendCommand  0xD5          ;Set display clock divide ratio/Oscillator frequency
+		SendCommand  0x80
+		SendCommand  0x8D          ;Charge pump command table
+		SendCommand  0x14
 		
-		  
-init:   Initialization
+		SendCommand  0x20;       
+		SendCommand  0x00
+		SendCommand  0x21;
+		SendCommand  0x00
+		SendCommand  0x7F
+		SendCommand  0x22;
+		SendCommand  0x00
+		SendCommand  0x07
+          
+		SendCommand  0xAF
+		;------Send DATA AND  !!!DON'T SEND STOP!!! IT WON'T WORK!!!
 		
-		;-----Initialization of displa
-		END: rjmp END
