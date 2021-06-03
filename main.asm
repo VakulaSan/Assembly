81ca07954f4a70fd993ae4f2f532f9c7ca606b5c
Synrone <vakulasan@gmail.com>
2021-06-03 12:13:22 +0300

    03.06.2021 morning Works fine

--- main.asm
+++ main.asm
@@ -6,19 +6,19 @@
           
 init:   Initialization
 		SendStart
-          SendSLAW 
+        SendSLAW 
 		;-----Initialization of display
 		SendCommand  0xA8          ;Set MUX ratio 3F
 		SendCommand  0x3F
 		SendCommand  0xD3          ;Set display offset
 		SendCommand  0x00
-          SendCommand  0x40          ;Display start line 40-7F
+        SendCommand  0x40          ;Display start line 40-7F
 		SendCommand  0xA0          ;Set Segment Re-map
 		SendCommand  0xC0          ;Set COM port output Scan detection
 		SendCommand  0xDA          ;Set COM Pins Hardware Configuration
 		SendCommand  0x12
 		SendCommand  0x81          ;Set contrast control 00-FF
-	     SendCommand  0xFF
+	    SendCommand  0xFF
 		SendCommand  0xA4          ;Entire display ON
 		SendCommand  0xA7          ;Set normal inverse display
 		SendCommand  0xD5          ;Set display clock divide ratio/Oscillator frequency
@@ -40,22 +40,22 @@ init:   Initialization
 		ClearScreen 0x78
 
 proceed:
-          clr     num_of_frames
+        clr     num_of_frames
 		inc     num_of_frames  
 		clr     inc_adresses
 animation:
-          mov     temp,    num_of_frames
-          cpi     temp,    0
+        mov     temp,    num_of_frames
+        cpi     temp,    0
 		breq    END		
 		dec     num_of_frames
-	     ldi     ZH,     high(array<<1)
+	    ldi     ZH,     high(array<<1)
 		ldi     ZL,     low(array<<1)
 		add     ZL,     inc_adresses
 		inc     inc_adresses
 		inc     inc_adresses
-	     push    inc_adresses
+	    push    inc_adresses
 		push    num_of_frames  
-          lpm     R26,    Z+
+        lpm     R26,    Z+
 		lpm     R27,    Z
 		ldi     temp,   2
 		mul     R26,    temp
@@ -66,23 +66,23 @@ animation:
 		add     R27,    R19
 		mov     ZH,     R27
 		mov     ZL,     R26
-          rcall   delay
+        rcall   delay
 		pop     num_of_frames
 		pop     inc_adresses
 		SendStart
 		SendSLAW 
 		
 		ldi     r18,    9         ; Set pages x = x-1. To set 8 pages you should set 9 e.g.
-send:     dec     r18
-          breq    animation
-          ldi     r19,    129
+send:   dec     r18
+        breq    animation
+        ldi     r19,    129
 lp:		dec     r19
-          breq    send
-          ldi     temp,   send_data
+        breq    send
+        ldi     temp,   send_data
 		sts     TWDR,   temp
 		ldi     temp,   1<<TWEN | 1<<TWINT 
 		sts     TWCR,   temp
-          rcall wait
+        rcall wait
 		lpm     temp,   Z+
 		sts     TWDR,   temp
 		ldi     temp,   1<<TWEN | 1<<TWINT 
